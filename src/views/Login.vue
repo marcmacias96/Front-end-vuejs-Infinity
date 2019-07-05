0380b728e61e0fc7a1a55ddd9ad9a1ae260a7f65
marcmacias96 <mamacias3@espe.edu.ec>
2019-07-05 15:10:42 -0500

    subida de archivos

---
+++ src/views/Login.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="container">
+        <form action="" method="POST" @submit.prevent="login">
+            <div class="form-group">
+                <input class="form-control" type="email" v-model="email" name="email" id="email" placeholder="Email">
+            </div>
+            <div class="form-group">
+                <input class="form-control" type="password" v-model="password"  name="password" id="password" placeholder="Password">
+            </div>
+            <div class="form-group">
+                <button type="submit" class="btn btn-primary" >Login </button>
+            </div>
+        </form>
+
+    </div> 
+</template>
+
+<script>
+import gql from  'graphql-tag'
+import { onLogin } from '../vue-apollo'
+
+export default {
+    data () {
+        return {
+            email : '',
+            password : ''
+        }
+    },
+    methods: {
+        login() {
+            this.$apollo.mutate({
+            // Query
+            mutation: gql`mutation ($input: SignInInput! ) {
+                signIn (input : $input) {
+                    token
+                }
+            }`,
+            // Parameters
+            variables: {
+                input : {
+                    email : this.email,
+                    password : this.password
+                }  
+            },
+            }).then((data) => {
+            // Result
+            console.log(data)
+            //onLogin(this.$apollo.provider.defaultClient, data.data.login.token)
+            this.$router.push('./Index.vue')
+            }).catch((error) => {
+            console.error(error)
+            console.log();
+            
+            })
+        },
+},
+}
+</script>
