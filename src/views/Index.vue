0da652728b43763bf1c590f68a4901a496356ada
Johan Zambrano <johanz97@hotmail.com>
2019-07-10 17:00:21 -0500

    subscripciones aplicadas a subida de imagenes lista (hay un error que no se donde es)

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,91 +1,100 @@
 <template >
- <div class="container p-5">
+  <div class="container p-5">
     <div class="card">
-    <div class="card-header bg-dark">
-      <h3 class="card-title text-white">
-        <i class="far fa-image"></i> Upload an Image
-      </h3>
-    </div>
+      <div class="card-header bg-dark">
+        <h3 class="card-title text-white">
+          <i class="far fa-image"></i> Upload an Image
+        </h3>
+      </div>
 
-    <div class="card-body">
-      <form action method="POST" @submit.prevent="cretaImage">
-        <div class="form-group">
-          <div class="input-group">
-            <div class="custom-file">
-              <input
-                @change="upload"
-                ref="file"
-                type="file"
-                name="image"
-                class="custom-file-input"
-                id="image"
-                required
-              />
-              <label class="custom-file-label" for="image">Choose file</label>
+      <div class="card-body">
+        <form action method="POST" @submit.prevent="cretaImage">
+          <div class="form-group">
+            <div class="input-group">
+              <div class="custom-file">
+                <input
+                  @change="upload"
+                  ref="file"
+                  type="file"
+                  name="image"
+                  class="custom-file-input"
+                  id="image"
+                  required
+                />
+                <label class="custom-file-label" for="image">Choose file</label>
+              </div>
+            </div>
+          </div>
+
+          <div class="form-group">
+            <input
+              type="text"
+              name="title"
+              class="form-control"
+              v-model="title"
+              placeholder="Title for the Image"
+              required
+            />
+          </div>
+          <div class="form-group">
+            <textarea
+              name="description"
+              rows="2"
+              class="form-control"
+              v-model="description"
+              placeholder="Deescription for the Image"
+              required
+            ></textarea>
+          </div>
+          <div class="form-group">
+            <button class="btn btn-success">
+              <i class="fa fa-upload"></i> Upload Image
+            </button>
+          </div>
+        </form>
+      </div>
+
+      <div class="card mt-2">
+        <div class="car-header bg-dark text-white">
+          <h3>
+            <i class="far fa-images">Recient Upload</i>
+          </h3>
+        </div>
+        <div class="card-body">
+          <div class="row">
+            <div class="col-md-4" :key="image.id" v-for="image in getImages">
+              <router-link :to="{name : 'viewImage', params: { id : image._id }}">
+                <img :src="url + image.fileName" alt=" " class="w-100 h-100 img-thumbnail" />
+              </router-link>
             </div>
           </div>
         </div>
-
-        <div class="form-group">
-          <input
-            type="text"
-            name="title"
-            class="form-control"
-            v-model="title"
-            placeholder="Title for the Image"
-            required
-          />
-        </div>
-        <div class="form-group">
-          <textarea
-            name="description"
-            rows="2"
-            class="form-control"
-            v-model="description"
-            placeholder="Deescription for the Image"
-            required
-          ></textarea>
-        </div>
-        <div class="form-group">
-          <button class="btn btn-success">
-            <i class="fa fa-upload"></i> Upload Image
-          </button>
-        </div>
-      </form>
-    </div>
-
-    <div class="card mt-2">
-      <div class="car-header bg-dark text-white">
-        <h3>
-          <i class="far fa-images">Recient Upload</i>
-        </h3>
-      </div>
-      <div class="card-body">
-          <div class="row">
-            <div class="col-md-4" :key="image.id"   v-for="image in getImages">
-                   
-              <router-link :to="{name : 'viewImage', params: { id : image._id }}">
-                <img :src="url + image.fileName" alt=" " class="w-100 h-100 img-thumbnail">   
-              </router-link> 
-                
-                
-            </div>
-           </div> 
       </div>
     </div>
   </div>
- </div>
 </template>
 
 <script>
 import gql from "graphql-tag";
+import { log } from 'util';
+
+const SUB_QUERY = gql`subscription photos{
+  photos {
+  _id
+  title
+  description
+  fileName
+  uniqueId
+  }
+}`;
+
 export default {
   data() {
     return {
       title: "",
       description: "",
       file: "",
-      url : "http://localhost:3000/static/upload/",
+      url: "http://localhost:3000/static/upload/"
     };
   },
   methods: {
@@ -93,7 +102,7 @@ export default {
       this.file = this.$refs.file.files[0];
     },
     cretaImage() {
-       this.$apollo.mutate({
+      this.$apollo.mutate({
         mutation: gql`
           mutation($input: ImageInput!, $usr_id: ID!) {
             createImage(input: $input, usr_id: $usr_id) {
@@ -107,27 +116,53 @@ export default {
             description: this.description,
             file: this.file
           },
-          usr_id: "5d22b0d15e51c91724684448"
+          usr_id: "5d262f4eef7d85219caaecc4"
         },
-        context: {
-          hasUpload: true
+        updateQueries : {
+          getImages :  (previousResult, { mutationResult }) => {
+            return {
+               getImages : [
+                  ...previousResult.getImages,
+                  mutationResult.data.photos
+               ]
+             }
+          }
         }
       });
-      this.$router.push('/index')
+      
     }
   },
   apollo: {
-    getImages: gql`
-      query {
-        getImages {
-          _id
-          title
-          description
-          fileName
-          uniqueId
-        }
-      }
-    `
+    getImages() {
+      return {
+        query: gql`
+          query getImages {
+            getImages {
+              _id
+              title
+              description
+              fileName
+              uniqueId
+            }
+          }
+        `,
+        fetchPolicy: "cache-and-network",
+
+        subscribeToMore: [{
+          document : SUB_QUERY,
+          updateQuery : (previousResult, { subscriptionData }) => {
+            
+            //this.$router.push({ name : 'viewImage', params: {id: subscriptionData.data.photos._id }})
+             return {
+               getImages : [
+                  ...previousResult.getImages,
+                  subscriptionData.data.photos
+               ]
+             }
+          }
+        }]
+      };
+    }
   }
 };
 </script>
--- src/vue-apollo.js
+++ src/vue-apollo.js
@@ -17,7 +17,7 @@ const defaultOptions = {
     httpEndpoint,
     // You can use `wss` for secure connection (recommended in production)
     // Use `null` to disable subscriptions
-    wsEndpoint: null,
+    wsEndpoint: 'ws://localhost:3000/graphql',
     // LocalStorage token
     tokenName: AUTH_TOKEN,
     // Enable Automatic Query persisting with Apollo Engine
